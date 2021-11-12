fa75d71a30ece09b69683eb4fdf70320a7b47966
Harish Manoharan <harishmanoharan17@gmail.com>
2021-11-12 10:29:07 +0530

    Create BA3D.py

---
+++ Chapter 3/BA3D.py
@@ -0,0 +1,21 @@
+def DeBruijn(k, Text):
+    over = {}
+    kmers = Composition(k, Text)
+    for i in kmers:
+        if i[:-1] not in over:
+            over[i[:-1]] = [i[1:]]
+        else:
+            over[i[:-1]].append(i[1:])
+    return over
+
+k = int(input())
+Text = input()
+over = DeBruijn(k, Text)
+for i in over:
+    s = ''
+    for j in over[i]:
+        if len(s) == 0:
+            s = j
+        else:
+            s = s + ',' + j
+    print(i, '->', s)
